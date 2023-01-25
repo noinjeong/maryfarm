9396cae800eaec676d041476a5c4133c9f2822b6
DataMarksman <zkstizn@gmail.com>
2023-01-25 17:48:11 +0900

    Feat: MyFarm_page_subcribe_btn making

---
+++ hwanseok/app/src/main/java/com/numberONE/maryfarm/Home/FarmFeed/FarmFeedActivity.java
@@ -0,0 +1,14 @@
+package com.numberONE.maryfarm.Home.FarmFeed;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FarmFeedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_farm_feed);
+    }
+}
