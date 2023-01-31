a29e40b32705926452992b7f0a0e0322387b07f0
kwanseob <foreverdunk@naver.com>
2023-01-31 17:44:32 +0900

    Feat : Detail Diary - touch button, screen transfer, harvest complete

--- MaryFarm/0.01v/MaryFarm/app/src/main/java/com/numberONE/maryfarm/Diary/DiaryDetailActivity.java
+++ MaryFarm/0.01v/MaryFarm/app/src/main/java/com/numberONE/maryfarm/Diary/DiaryDetailActivity.java
@@ -1,13 +1,17 @@
 package com.numberONE.maryfarm.Diary;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -16,9 +20,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.numberONE.maryfarm.Pick.PickActivity;
 import com.numberONE.maryfarm.R;
 import com.numberONE.maryfarm.databinding.ActivityDiaryDetailBinding;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,8 +38,7 @@ public class DiaryDetailActivity extends AppCompatActivity implements PopupMenu.
 
     // 팝업 메뉴창 구현 (일지 추가하기, 수정하기, 재배완료 선택)
     ImageButton popUpBtn;
-
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -72,6 +78,17 @@ public class DiaryDetailActivity extends AppCompatActivity implements PopupMenu.
                 }
             }
         });
+
+        // 추천 버튼 클릭시, 추천 페이지로 화면 이동
+        ImageButton pickBtn = (ImageButton) findViewById(R.id.pickBtn);
+        pickBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(DiaryDetailActivity.this, PickActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
         
         // 댓글목록 리싸이클러뷰 출력
         RecyclerView recyclerView = findViewById(R.id.commentsView);
@@ -86,6 +103,7 @@ public class DiaryDetailActivity extends AppCompatActivity implements PopupMenu.
         recyclerView.setAdapter(new CommentAdapter(getApplicationContext(),items));
     }
 
+    // ... 버튼 클릭시 팝업 메뉴 출력 (일지 수정, 일지 추가, 지배완료)
     public void showPopBtn(View v) {
         PopupMenu popupMenu = new PopupMenu(this, v);
         popupMenu.setOnMenuItemClickListener(this);
@@ -96,13 +114,23 @@ public class DiaryDetailActivity extends AppCompatActivity implements PopupMenu.
     public boolean onMenuItemClick(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.editDiary:
-                Toast.makeText(this, "일지 수정하기!", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(DiaryDetailActivity.this, DiaryModifyActivity.class);
+                startActivity(intent);
+                finish();
                 return true;
             case R.id.addDiary:
-                Toast.makeText(this, "일지 작성하기!", Toast.LENGTH_LONG).show();
+                Intent intent1 = new Intent(DiaryDetailActivity.this, DiaryAddActivity.class);
+                startActivity(intent1);
+                finish();
                 return true;
             case R.id.plantComplete:
-                Toast.makeText(this, "재배완료!", Toast.LENGTH_LONG).show();
+                Toast.makeText(this, "🌱🌻🌼 축 재배완료! 🥕🥦🌶", Toast.LENGTH_LONG).show();
+                String koreaNow = LocalDate.now(ZoneId.of("Asia/Seoul")).toString();
+                Log.d("dd", "korea date "+koreaNow);
+
+                TextView endDate = findViewById(R.id.endDate);
+                endDate.setText(koreaNow);
+
                 return true;
             default:
                 return false;
