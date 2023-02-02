a465843459d5b67a4e22b24ec5815da44d7a2bdb
김이삭 <40036437+Isak-toast@users.noreply.github.com>
2023-02-02 17:15:33 +0900

    Dev: deploy test

--- BackEnd/microfarmK/maryfarm-notify-service/src/main/java/com/ssafy/maryfarmnotifyservice/kafka/constants/KafkaConstants.java
+++ BackEnd/microfarmK/maryfarm-notify-service/src/main/java/com/ssafy/maryfarmnotifyservice/kafka/constants/KafkaConstants.java
@@ -2,5 +2,5 @@ package com.ssafy.maryfarmnotifyservice.kafka.constants;
 
 public class KafkaConstants {
     public static final String GROUP_ID = "notify-service";
-    public static final String KAFKA_BROKER = "127.0.0.1:9092";
+    public static final String KAFKA_BROKER = "172.18.0.101:9092";
 }
