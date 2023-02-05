3e0fa9b28198dda163e64265ac878de9e9f02c0f
DataMarksman <zkstizn@gmail.com>
2023-02-06 05:27:54 +0900

    Feat: Pick_Algo on_going

--- FrontEnd/MaryFarm/0.01v/MaryFarm/app/src/main/java/com/numberONE/maryfarm/ui/myfarm/MyfarmFragment.java
+++ FrontEnd/MaryFarm/0.01v/MaryFarm/app/src/main/java/com/numberONE/maryfarm/ui/myfarm/MyfarmFragment.java
@@ -6,12 +6,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.numberONE.maryfarm.Diary.DiaryAddActivity;
 import com.numberONE.maryfarm.Diary.DiaryDetailActivity;
+import com.numberONE.maryfarm.Pick.PickAlgorithm;
 import com.numberONE.maryfarm.R;
 
 
@@ -79,9 +82,17 @@ public class MyfarmFragment extends Fragment {
 
 
 
-    // 버튼 클릭시 디테일 화면으로 이동
+
     private View view;
+
+    // 버튼 클릭시 디테일 화면으로 이동
     private Button detailBtn;
+
+    // 취향 추천 알고리즘 페이지로 리다이렉트 시키는 버튼인데, 임시로 첫글 작성 버튼으로 만듦
+    private ImageButton recommendBtn;
+
+    // 취향 추천 알고리즘 페이지로 리다이렉트 시키는 버튼
+    private ImageButton recommendMonthBtn;
 
     @Nullable
     @Override
@@ -89,6 +100,8 @@ public class MyfarmFragment extends Fragment {
         view = inflater.inflate(R.layout.fragment_myfarm_profile,container,false);
 
         detailBtn = (Button) view.findViewById(R.id.detailBtn);
+        recommendBtn = (ImageButton) view.findViewById(R.id.recommendBtn);
+        recommendMonthBtn = (ImageButton) view.findViewById(R.id.recommendMonthBtn);
 
         detailBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -96,9 +109,27 @@ public class MyfarmFragment extends Fragment {
                 Intent intent = new Intent(getActivity(), DiaryDetailActivity.class); //fragment라서 activity intent와는 다른 방식
                 intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
                 startActivity(intent);
+            }
+        });
 
+        recommendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), DiaryAddActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivity(intent);
+            }
+        });
+
+        recommendMonthBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), PickAlgorithm.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivity(intent);
             }
         });
         return view;
     }
+
 }
