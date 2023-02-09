b0730e7996e037bfe004a1b647a610eaa661fd3b
jungboke <tmdqjaqhdl3@naver.com>
2023-02-09 21:48:02 +0900

    0209

--- BackEnd/microfarm/maryfarm-chat-service/src/main/java/com/ssafy/maryfarmchatservice/repository/RoomRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.maryfarmchatservice.repository;
-
-import com.ssafy.maryfarmchatservice.domain.chat.Room;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoomRepository extends JpaRepository<Room, String> {
-}
