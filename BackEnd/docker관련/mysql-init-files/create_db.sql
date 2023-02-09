9da6b75cb23ec1257be1700adcbd1703c25ef69e
jungboke <tmdqjaqhdl3@naver.com>
2023-02-09 21:43:39 +0900

    0209

--- BackEnd/docker관련/mysql-init-files/create_db.sql
+++
@@ -1,6 +0,0 @@
-create database notifydb;
-create database userdb;
-create database plantdb;
-create database boarddb;
-create database chatdb;
-create database calendardb;
--- BackEnd/myFarm/src/main/java/com/ssafy/myfarm/repository/RecommendRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.myfarm.repository;
-
-import com.ssafy.myfarm.domain.user.Recommend;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RecommendRepository extends JpaRepository<Recommend, String> {
-}
