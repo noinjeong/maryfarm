36ecc0694c80f77a68829bbf63e55a9a55a7c212
robotdiary <molihua321.dp@gmail.com>
2023-02-09 17:32:34 +0900

    Feat: ChatFrgment

--- FrontEnd/MaryFarm/0.01v/MaryFarm/app/src/main/java/com/numberONE/maryfarm/ui/chat/ChatFragment.java
+++ FrontEnd/MaryFarm/0.01v/MaryFarm/app/src/main/java/com/numberONE/maryfarm/ui/chat/ChatFragment.java
@@ -21,45 +21,24 @@ import com.numberONE.maryfarm.databinding.FragmentChatBinding;
 import java.net.Socket;
 import java.net.URISyntaxException;
 
-//import io.socket.client.IO;
-//import io.socket.client.Socket;
+import ua.naiksoftware.stomp.Stomp;
+import ua.naiksoftware.stomp.StompClient;
+
 
 public class ChatFragment extends Fragment {
 
-    private FragmentChatBinding binding;
-    private Socket mSocket;
-    private String username;
-    private String roomNumber;
-
+    FragmentChatBinding binding;
+//    String url = "ws://example.com:8080/socket/websocket"; // 소켓에 연결하는 엔드포인트가 /socket일때 다음과 같음
+//    StompClient stompClient = Stomp.over(Stomp.ConnectionProvider.OKHTTP, url);
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        ChatViewModel dashboardViewModel =
-                new ViewModelProvider(this).get(ChatViewModel.class);
 
         binding = FragmentChatBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-//         init();
-
         return root;
     }
-
-//    private void init() {
-//        try {
-//            mSocket = IO.socket("http://10.0.2.2:80/");
-//            Log.d("SOCKET", "Connection success : " + mSocket.id());
-//        } catch (URISyntaxException e) {
-//            e.printStackTrace();
-//        }
-//
-//        Intent intent = getActivity().getIntent(); //getIntent()만 안 되서 바꿔봄
-//        username = intent.getStringExtra("username");
-//
-//        mSocket.connect();
-//    }
 
     @Override
     public void onDestroyView() {
