d3a78ab047f81aaba5010db0ca06a33ef7b66dd3
kwanseob <foreverdunk@naver.com>
2023-02-10 14:33:57 +0900

    Feat : Myfarm Fragment - server connection

---
+++ FrontEnd/MaryFarm/0.01v/MaryFarm/app/src/main/java/com/numberONE/maryfarm/Retrofit/dto/DetailDiariesPerPlantView/DetailDiaryCommentDTO.java
@@ -0,0 +1,9 @@
+package com.numberONE.maryfarm.Retrofit.dto.DetailDiariesPerPlantView;
+
+
+public class DetailDiaryCommentDTO {
+    private String profilePath;
+    private String userName;
+    private String content;
+    private Integer likes;
+}
