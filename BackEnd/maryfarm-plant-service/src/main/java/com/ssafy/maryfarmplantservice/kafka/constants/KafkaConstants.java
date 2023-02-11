43fa5977c13c8a0703ccf963504e58357ba07411
jungboke <tmdqjaqhdl3@naver.com>
2023-02-11 22:36:35 +0900

    230211

--- BackEnd/maryfarm-plant-service/src/main/java/com/ssafy/maryfarmplantservice/kafka/constants/KafkaConstants.java
+++
@@ -1,6 +0,0 @@
-package com.ssafy.maryfarmplantservice.kafka.constants;
-
-public class KafkaConstants {
-    public static final String GROUP_ID = "plant-service";
-    public static final String KAFKA_BROKER = "127.0.0.1:9092";
-}
--- BackEnd/maryfarm-user-service/src/main/java/com/ssafy/maryfarmuserservice/jpa_repository/FollowRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ssafy.maryfarmuserservice.jpa_repository;
-
-import com.ssafy.maryfarmuserservice.domain.user.Follow;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FollowRepository extends JpaRepository<Follow, String> {
-
-}
