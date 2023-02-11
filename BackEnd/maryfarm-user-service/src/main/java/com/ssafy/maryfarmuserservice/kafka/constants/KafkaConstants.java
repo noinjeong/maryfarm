90b10e3506f1c25bb4aaf00538c4f87672062644
jungboke <tmdqjaqhdl3@naver.com>
2023-02-11 22:57:37 +0900

    0211

--- BackEnd/maryfarm-user-service/src/main/java/com/ssafy/maryfarmuserservice/kafka/constants/KafkaConstants.java
+++
@@ -1,6 +0,0 @@
-package com.ssafy.maryfarmuserservice.kafka.constants;
-
-public class KafkaConstants {
-    public static final String GROUP_ID = "user-service";
-    public static final String KAFKA_BROKER = "127.0.0.1:9092";
-}
