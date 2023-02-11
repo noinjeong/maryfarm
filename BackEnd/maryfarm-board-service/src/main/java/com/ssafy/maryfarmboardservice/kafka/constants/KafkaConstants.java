58cd492a454a7c4074d5f780a64d9d44b4e7f97c
jungboke <tmdqjaqhdl3@naver.com>
2023-02-11 23:03:27 +0900

    0211

--- BackEnd/maryfarm-board-service/src/main/java/com/ssafy/maryfarmboardservice/kafka/constants/KafkaConstants.java
+++
@@ -1,6 +0,0 @@
-package com.ssafy.maryfarmboardservice.kafka.constants;
-
-public class KafkaConstants {
-    public static final String GROUP_ID = "board-service";
-    public static final String KAFKA_BROKER = "127.0.0.1:9092";
-}
--- BackEnd/maryfarm-calendar-service/src/main/java/com/ssafy/maryfarmcalendarservice/kafka/constants/KafkaConstants.java
+++
@@ -1,6 +0,0 @@
-package com.ssafy.maryfarmcalendarservice.kafka.constants;
-
-public class KafkaConstants {
-    public static final String GROUP_ID = "calendar-service";
-    public static final String KAFKA_BROKER = "127.0.0.1:9092";
-}
