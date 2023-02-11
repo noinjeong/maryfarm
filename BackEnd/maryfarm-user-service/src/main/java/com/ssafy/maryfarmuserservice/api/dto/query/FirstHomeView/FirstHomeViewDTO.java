246737a196a97e945b33e2b9db9a84f8b2bb3e5a
jungboke <tmdqjaqhdl3@naver.com>
2023-02-12 01:33:48 +0900

    0212
    Former-commit-id: 252c76f1039472e117846c3c30b08b324368070e

--- BackEnd/maryfarm-user-service/src/main/java/com/ssafy/maryfarmuserservice/api/dto/query/FirstHomeView/FirstHomeViewDTO.java
+++ BackEnd/maryfarm-user-service/src/main/java/com/ssafy/maryfarmuserservice/api/dto/query/FirstHomeView/FirstHomeViewDTO.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Document(collection = "FirstHomeView")
-@Data 
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class FirstHomeViewDTO {
