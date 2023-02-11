d582ce48dd4965ee5bf0c3b871daec2374d09411
jungboke <tmdqjaqhdl3@naver.com>
2023-02-11 23:02:39 +0900

    0211

--- BackEnd/maryfarm-user-service/src/main/java/com/ssafy/maryfarmuserservice/jpa_repository/RecommendRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.maryfarmuserservice.jpa_repository;
-
-import com.ssafy.maryfarmuserservice.domain.user.Recommend;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RecommendRepository extends JpaRepository<Recommend, String> {
-}
