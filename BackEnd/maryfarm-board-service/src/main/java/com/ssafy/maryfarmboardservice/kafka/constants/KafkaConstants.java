a97db5beb13f0c6329d35ff5b2060d4cf8e57402
jungboke <tmdqjaqhdl3@naver.com>
2023-02-14 16:38:16 +0900

    Debug: API 3차 보수

--- BackEnd/maryfarm-board-service/src/main/java/com/ssafy/maryfarmboardservice/kafka/constants/KafkaConstants.java
+++ BackEnd/maryfarm-board-service/src/main/java/com/ssafy/maryfarmboardservice/kafka/constants/KafkaConstants.java
@@ -2,5 +2,6 @@ package com.ssafy.maryfarmboardservice.kafka.constants;
 
 public class KafkaConstants {
     public static final String GROUP_ID = "board-service";
-    public static final String KAFKA_BROKER = "127.0.0.1:9092";
+//    public static final String KAFKA_BROKER = "127.0.0.1:9092";
+    public static final String KAFKA_BROKER = "kafka:9092";
 }
