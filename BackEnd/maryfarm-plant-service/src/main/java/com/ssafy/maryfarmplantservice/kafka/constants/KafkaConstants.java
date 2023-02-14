8fdfe248d0cbddd9bdf71e0b9c0395b90b0287a3
jungboke <tmdqjaqhdl3@naver.com>
2023-02-14 17:48:15 +0900

    아래꺼 바로

--- BackEnd/maryfarm-plant-service/src/main/java/com/ssafy/maryfarmplantservice/kafka/constants/KafkaConstants.java
+++ BackEnd/maryfarm-plant-service/src/main/java/com/ssafy/maryfarmplantservice/kafka/constants/KafkaConstants.java
@@ -2,6 +2,6 @@ package com.ssafy.maryfarmplantservice.kafka.constants;
 
 public class KafkaConstants {
     public static final String GROUP_ID = "plant-service";
-    public static final String KAFKA_BROKER = "127.0.0.1:9092";
-//    public static final String KAFKA_BROKER = "kafka:9092";
+//    public static final String KAFKA_BROKER = "127.0.0.1:9092";
+    public static final String KAFKA_BROKER = "kafka:9092";
 }
