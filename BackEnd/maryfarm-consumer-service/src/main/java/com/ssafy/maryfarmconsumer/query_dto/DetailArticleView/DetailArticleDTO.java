13a9f20d44a89b0b920612a921bb065ae62134a8
jungboke <tmdqjaqhdl3@naver.com>
2023-02-16 16:11:26 +0900

    230216

--- BackEnd/maryfarm-consumer-service/src/main/java/com/ssafy/maryfarmconsumer/query_dto/DetailArticleView/DetailArticleDTO.java
+++ BackEnd/maryfarm-consumer-service/src/main/java/com/ssafy/maryfarmconsumer/query_dto/DetailArticleView/DetailArticleDTO.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.TimeZone;
 
-@Document(collection = "DetailArticle") 
+@Document(collection = "DetailArticle")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
